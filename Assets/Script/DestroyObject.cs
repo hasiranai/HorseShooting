0ddaf00baeb0e4dd74af31fbc07d6fb662c8a40a
河内真輝 <>
2024-04-10 05:43:23 +0900

    手順８部分

--- Assets/Script/DestroyObject.cs
+++ Assets/Script/DestroyObject.cs
@@ -4,14 +4,39 @@ using UnityEngine;
 
 public class DestroyObject : MonoBehaviour
 {
+    public GameObject effectPrefab;
+    public GameObject effectPrefab2;
+    public int objectHP;
+
     // このメソッドはコライダー同士がぶつかった瞬間に呼び出される
     private void OnTriggerEnter(Collider other)
     {
         // もしもぶつかった相手のTagにShellという名前が書いてあったならば（条件）
         if (other.CompareTag("Shell"))
         {
-            // このスクリプトがついているオブジェクトを破壊する
-            Destroy(other.gameObject);
+            // オブジェクトのHPを１ずつ減少させる
+            objectHP -= 1;
+
+            // もしもHPが0よりも大きい場合には（条件）
+            if (objectHP > 0)
+            {
+                // このスクリプトがついているオブジェクトを破壊する
+                Destroy(other.gameObject);
+
+                GameObject effect = Instantiate(effectPrefab, other.transform.position, Quaternion.identity);
+
+                Destroy(effect, 2.0f);
+            }
+            else    // そうでない場合（HPが0以下になった場合）には（条件）
+            {
+                Destroy(other.gameObject);
+
+                // もう１種類のエフェクトを発生させる
+                GameObject effect2 = Instantiate(effectPrefab2, other.transform.position, Quaternion.identity);
+
+                Destroy(effect2, 2.0f);
+                Destroy(this.gameObject);
+            }
         }
     }
 }
