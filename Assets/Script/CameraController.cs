f8acabd03ded2bac84be1caa062908d67d9f9c44
河内真輝 <>
2024-04-16 05:09:51 +0900

    手順１５部分（カメラ切り替え）

--- Assets/Script/CameraController.cs
+++ Assets/Script/CameraController.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Camera fpsCamera;
+
+    // 初期値は-1
+    private int num = -1;
+
     void Start()
     {
-        
+        // FPSカメラのdepthの初期値を「-1」に設定（Mainカメラが「０」なので、最初はMainカメラが優先）
+        fpsCamera.depth = num;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            // Cボタンを押すごとに、numの値が「１」と「−１」に変化する（テクニック）
+            num *= -1;
+
+            // depthが「１」の時・・・＞FPSカメラが優先
+            // depthが「−１」の時・・・＞Mainカメラ優先
+            fpsCamera.depth = num;
+        }
     }
 }
